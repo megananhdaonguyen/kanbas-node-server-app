179a16ee2b0244d7c159e071c9e18141ebbc1a2d
megananhdaonguyen <nguyen.meg@northeastern.edu>
2024-04-04 03:16:15 -0400

    completed assignment

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,5 +1,39 @@
 import Database from "../Database/index.js";
 export default function CourseRoutes(app) {
+  app.put("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    const moduleIndex = db.modules.findIndex(
+      (m) => m._id === mid);
+    db.modules[moduleIndex] = {
+      ...db.modules[moduleIndex],
+      ...req.body
+    };
+    res.sendStatus(204);
+  });
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses
+      .find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = req.body;
+    Database.courses = Database.courses.map((c) =>
+      c._id === id ? { ...c, ...course } : c
+    );
+    res.sendStatus(204);
+  });
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    Database.courses = Database.courses
+      .filter((c) => c._id !== id);
+    res.sendStatus(204);
+  });
   app.post("/api/courses", (req, res) => {
     const course = {
       ...req.body,
