01735dee23a43c8f3dcda42adcb6d04241a72779
megananhdaonguyen <nguyen.meg@northeastern.edu>
2024-04-03 23:07:09 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./Kanbas/Courses/routes.js"
+const app = express()
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+Lab5(app);
+Hello(app)
+app.listen(process.env.PORT || 4000)
