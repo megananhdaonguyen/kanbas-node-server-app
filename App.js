e47f95c4198934bc915faebd98c75a5653e5dcda
megananhdaonguyen <nguyen.meg@northeastern.edu>
2024-04-04 03:35:04 -0400

    small fix

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js"
+import CourseRoutes from "./Kanbas/Courses/routes.js"
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 const app = express()
 app.use(cors());
